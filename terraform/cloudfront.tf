88384df15dc61fe57e4c6c2b58d160486339a0c3
olumercy <74353020+olumercy@users.noreply.github.com>
2025-05-27 01:26:54 +0000

    modi6

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -96,9 +96,11 @@ data "aws_iam_policy_document" "cloud_talent_blog" {
         condition {
             test = "StringEquals"
             variable = "AWS:SourceArn" 
-            values = <<EOT [
-              "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.cloud_talent_CDN.id}
-              ] EOT
+            values =  <<EOT
+            [
+                "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.cloud_talent_CDN.id}"
+           ]
+           EOT
              
     }
 }
