762f60119b63a06cf6abc170a9d4a71f5386a9f0
olumercy <74353020+olumercy@users.noreply.github.com>
2025-05-27 01:50:40 +0000

    mode1

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -96,8 +96,7 @@ data "aws_iam_policy_document" "cloud_talent_blog" {
         condition {
             test = "StringEquals"
             variable = "AWS:SourceArn" 
-            values =  
-            [
+            values =  [
                 "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.cloud_talent_CDN.id}"
            ]
            
