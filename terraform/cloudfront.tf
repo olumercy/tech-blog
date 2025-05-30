d287e30fc59c4a0c3cf2a07e78aa6f9f73a27fb6
olumercy <74353020+olumercy@users.noreply.github.com>
2025-05-30 01:27:36 +0000

    cha3

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -11,17 +11,20 @@ resource "aws_cloudfront_function" "add_index_html" {
   name    = "AddIndexHtmlFunction"
   runtime = "cloudfront-js-2.0"
   publish = true
-  code    = file("code/function.js")
-  #function handler(event) {
-  #  var request = event.request;
+  code    = <<EOF
+  function handler(event) {
+    var request = event.request;
 
-  #  if (!request.uri.includes('.') && !request.uri.endsWith('/')) {
-  #    request.uri += '/index.html';
-  #  }
+    if (!request.uri.includes('.')) {
+      request.uri += '/index.html';
+    }
+    else if (!request.uri.endsWith('/')) {
+      request.uri += '/index.html';
+    }
 
-  #  return request;
-  #}
- # EOF
+    return request;
+  }
+  EOF
 }
 
 
